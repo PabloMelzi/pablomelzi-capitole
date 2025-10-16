d7e0e996e4e8225e174c0df211ce18435822d768
PabloMelzi <paul281404@gmail.com>
2025-10-16 02:42:43 +0200

    Entity Modifications

--- src/main/java/com/pablomelzi/prueba/infrastructure/out/persistence/PriceEntity.java
+++ src/main/java/com/pablomelzi/prueba/infrastructure/out/persistence/PriceEntity.java
@@ -3,6 +3,10 @@ package com.pablomelzi.prueba.infrastructure.out.persistence;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,13 +19,30 @@ public class PriceEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     private Long brandId;
+
+    @NotNull
     private LocalDateTime startDate;
+
+    @NotNull
     private LocalDateTime endDate;
+
+    @NotNull
     private Integer priceList;
+
+    @NotNull
     private Long productId;
+
+    @NotNull
+    @Min(0)
     private Integer priority;
+
+    @NotNull
+    @DecimalMin("0.0")
     private BigDecimal price;
+
+    @NotNull
     private String currency;
 
 }
